f515c2809b852a17ac46398e416dd58edf34f928
shhm8905 <hazem-khalil@hotmail.com>
2022-06-06 23:18:49 +0200

    fix enviroment variables in firebase

--- src/Firebase/index.js
+++ src/Firebase/index.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIRE_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_SENDER_ID,
-    appId: process.env.REACT_APP_AP_ID
+    apiKey: process.env?.REACT_APP_FIRE_KEY,
+    authDomain: process.env?.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env?.REACT_APP_PROJECT_ID,
+    storageBucket: process.env?.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env?.REACT_APP_SENDER_ID,
+    appId: process.env?.REACT_APP_AP_ID
 };
 
 const app = initializeApp(firebaseConfig);
