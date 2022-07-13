732b343a078ea5cc47e27449d38428198b749683
shhm8905 <hazem-khalil@hotmail.com>
2022-07-13 23:48:01 +0200

    Update inviroment variabels

--- src/api/user.js
+++ src/api/user.js
@@ -1,33 +1,36 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API = axios.create({ baseURL: 'https://higm-project.herokuapp.com/users' });
+const API = axios.create({ baseURL: process.env?.REACT_APP_HERO_USER });
 
-const user = JSON.parse(localStorage.getItem('user'));
+const user = JSON.parse(localStorage.getItem("user"));
 
-API.interceptors.request.use(req => {
-    if (user) {
-        req.headers.auhtorization = `Bearer ${user.token}`;
-    }
-    return req;
+API.interceptors.request.use((req) => {
+  if (user) {
+    req.headers.auhtorization = `Bearer ${user.token}`;
+  }
+  return req;
 });
 
 const signUp = async (userData) => {
-    const res = await API.post('/signup', userData).catch(err => console.log(err));
-    res.data && localStorage.setItem('user', JSON.stringify(res.data));
-    return res.data;
-}
+  const res = await API.post("/signup", userData).catch((err) =>
+    console.log(err)
+  );
+  res.data && localStorage.setItem("user", JSON.stringify(res.data));
+  return res.data;
+};
 
 const signIn = async (userData) => {
-    const res = await API.post('/signin', userData).catch(err => console.log(err));
-    res.data && localStorage.setItem('user', JSON.stringify(res.data));
-    return res.data;
-}
+  const res = await API.post("/signin", userData).catch((err) =>
+    console.log(err)
+  );
+  res.data && localStorage.setItem("user", JSON.stringify(res.data));
+  return res.data;
+};
 
 const googleLogin = (userData) => {
-    userData && localStorage.setItem('user', JSON.stringify(userData));
-    return JSON.parse(localStorage.getItem('user'));
-}
-
+  userData && localStorage.setItem("user", JSON.stringify(userData));
+  return JSON.parse(localStorage.getItem("user"));
+};
 
 const Auth = { signIn, signUp, googleLogin };
 
